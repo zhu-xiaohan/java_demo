49117c22b04cf219e0d7214e02a0801a93ba4eb0
zhu-xiaohan <2705576102@qq.com>
2016-09-08 18:10:50 +0800

    10000以内的和

---
+++ demo1/src/main/java/Text7.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Asus on 2016/9/8.
+ */
+public class Text7 {
+
+    public static void main(String[] agse) {
+        int nun = 0;
+
+        int sun;
+        for(sun = 0; nun <= 10000; nun += 2) {
+            sun += nun;
+        }
+
+        System.out.println("sun:" + sun);
+    }
+}
